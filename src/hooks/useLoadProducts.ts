ee43b29017a37957100b31d339f55880b361fadb
Tbence98 <tlrbence@gmail.com>
2022-12-10 20:57:32 +0100

    add infinite scroll

--- src/hooks/useLoadProducts.ts
+++ src/hooks/useLoadProducts.ts
@@ -2,48 +2,54 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 type Product = {
-    id: number,
-    title: string,
-    description: string,
-    price: number,
-    discountPercentage: number,
-    rating: number,
-    stock: number,
-    brand: string,
-    category: string,
-    thumbnail: string,
-    images: string[]
-}
-
-type GetProductsResponse = {
-  products: Product[],
-  total: number,
-  skip: number,
-  limit: number
+    id: number;
+    title: string;
+    description: string;
+    price: number;
+    discountPercentage: number;
+    rating: number;
+    stock: number;
+    brand: string;
+    category: string;
+    thumbnail: string;
+    images: string[];
 };
 
-const useLoadProducts = (query: number, pageNumber: number) => {
+type GetProductsResponse = {
+    products: Product[];
+    total: number;
+    skip: number;
+    limit: number;
+};
+
+const useLoadProducts = (query: number, skipNumber: number) => {
     const [products, setProducts] = useState<Product[]>([]);
     const [hasMore, setHasMore] = useState(false);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
 
     useEffect(() => {
-    axios<GetProductsResponse>({
-      method: 'GET',
-      url: 'https://dummyjson.com/products',
-      params: { limit: query, skip: pageNumber },
-    }).then((res) => {
-        setProducts(prevProducts => {
-          return [...prevProducts, ...res.data.products];
-        });
-        setHasMore(res.data.total > pageNumber);
-    }).catch(e => {
-        setError(true);
-    })
-    }, [query, pageNumber])
+        setLoading(true);
+        setError(false);
+        axios<GetProductsResponse>({
+            method: "GET",
+            url: "https://dummyjson.com/products",
+            params: { limit: query, skip: skipNumber },
+        })
+            .then((res) => {
+                setProducts((prevProducts) => {
+                    return [...prevProducts, ...res.data.products];
+                });
+                setHasMore(res.data.total > skipNumber);
+                setLoading(false);
+            })
+            .catch((e) => {
+                setError(true);
+                setLoading(false);
+            });
+    }, [query, skipNumber]);
 
     return { products, loading, error, hasMore };
-}
+};
 
 export default useLoadProducts;
--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,31 +1,45 @@
-import { useState} from 'react';
-import useLoadProducts from '../hooks/useLoadProducts';
+import { useState, useRef, useCallback } from "react";
+import useLoadProducts from "../hooks/useLoadProducts";
 
 const Products = () => {
-    const [pageNum, setPageNum] = useState(0);
-    const {
-        products,
-        hasMore,
-        loading,
-        error
-    } = useLoadProducts(10, pageNum);
+    const [skipNum, setSkipNum] = useState(0);
+    const { products, hasMore, loading, error } = useLoadProducts(10, skipNum);
 
-    const incPageNum = () => {
-        setPageNum(prevNum => prevNum + 10);
-    }
+    const observer = useRef<IntersectionObserver>();
+
+    const lastProductElementRef = useCallback(
+        (node: HTMLLIElement) => {
+            if (loading) return;
+            if (observer.current) observer.current.disconnect();
+            observer.current = new IntersectionObserver((entries) => {
+                if (entries[0].isIntersecting && hasMore) {
+                    setSkipNum((prevPageNumber) => prevPageNumber + 10);
+                }
+            });
+            if (node) observer.current.observe(node);
+        },
+        [loading, hasMore]
+    );
 
     console.log(products);
     return (
         <>
-        <h1>Hello World</h1>
-        <ul>
-            {products.map(product => {
-            return (<li key={product.id}>{product.title}</li>)
-            })}
-        </ul>
-        <button onClick={incPageNum}>Increase</button>
+            <h1>Hello World</h1>
+            <ul>
+                {products.map((product, index) => {
+                    if (products.length === index + 1) {
+                        return (
+                            <li ref={lastProductElementRef} key={product.id}>
+                                {product.title}
+                            </li>
+                        );
+                    } else {
+                        return <li key={product.id}>{product.title}</li>;
+                    }
+                })}
+            </ul>
         </>
-    )
-}
+    );
+};
 
 export default Products;
