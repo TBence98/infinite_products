084bfb1f5dbcad1f5ab3b62fbda78668bf200ac9
Tbence98 <tlrbence@gmail.com>
2022-12-13 15:11:35 +0100

    remove strictMode at main.tsx temporary

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,20 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+/*
+    StrictMode is removed now because it causes 2 renders when the components
+    mount and the first ten products load 2x unnecessarily and also causing
+    problem with their keys. This does not affect production mode,
+    but I removed it anyways therefore you can test the app with the development
+    server. If you want StrictMode just replace root creation lines with the
+    code below*/
+
+/* ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <App />
     </React.StrictMode>
+);*/
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <App />
 );
